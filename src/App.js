87c78d48f4574ce258b99632e66f4ae6af2ec71d
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2021-08-15 15:47:27 -0400

    mvp Adds basic output tables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import MUIDataTable from "mui-datatables";
 import messages from './constants/messages';
+import {
+  removeLeadingIntegersFromKeys,
+  transformBestMatches,
+  transformHistoricalPrices,
+  transformSMA,
+  replaceSpacesWithDashes
+} from './utilities/transformations'
 import { useState, useEffect } from 'react';
 
 const App = () => {
@@ -8,8 +15,11 @@ const App = () => {
   const [keyword, setKeyword] = useState('');
   const [securitiesData, setSecuritiesData] = useState([]);
   const [selectedSecuritySymbol, setSelectedSecuritySymbol] = useState(null);
-  const [selectedSecuritySymbolData, setSelectedSecuritySymbolData] = useState([]);
+  const [selectedSecuritySymbol5MinuteData, setSelectedSecuritySymbol5MinuteData] = useState([]);
+  const [selectedSecuritySymbol60MinuteData, setSelectedSecuritySymbol60MinuteData] = useState([]);
+  const [selectedSecuritySymbolIndicatorSMAData, setSelectedSecuritySymbolIndicatorSMAData] = useState([]);
   const [apiKey, setApiKey] = useState('');
+  const [globalQuoteInfo, setGlobalQuoteInfo] = useState({});
 
   // TODO: finish this logic
   useEffect(() => {
@@ -17,28 +27,55 @@ const App = () => {
     setApiKey(apiKey);
   }, []);
 
-  // TODO: generalize to a helper function
-  const transformSecuritiesData = bestMatches => {
-    return bestMatches.map(item => {
-      const obj = {};
-      for (const keyWithNumber in item) {
-        const [, key] = keyWithNumber.split(/\. /);
-        obj[key] = item[keyWithNumber];
-      }
-      return obj;
-    });
-  }
-
-  const getSymbols = (event) => {
+  const getSecurities = (event) => {
     event.preventDefault();
     const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=${apiKey}`
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        setSecuritiesData(transformSecuritiesData(data.bestMatches));
+        setSecuritiesData(transformBestMatches(data.bestMatches));
+      })
+      // TODO: proper error handling
+      .catch(console.log)
+  };
+
+  const getHistoricalPrices = (symbol, interval, setterFn) => {
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=${interval}&apikey=${apiKey}`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const timeSeriesData = data[`Time Series (${interval})`];
+        const transformedData = transformHistoricalPrices(timeSeriesData);
+        setterFn(transformedData);
+      })
+      // TODO: proper error handling
+      .catch(console.log)
+  };
+
+  const getSMA = (symbol, interval) => {
+    const url = `https://www.alphavantage.co/query?function=SMA&symbol=${symbol}&interval=${interval}&time_period=60&series_type=open&apikey=${apiKey}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        // debugger;
+        const transformed = transformSMA(data['Technical Analysis: SMA']);
+        setSelectedSecuritySymbolIndicatorSMAData(transformed);
+      })
+      // TODO: proper error handling
+      .catch(console.log)
+  };
+
+  const getGlobalQuote = symbol => {
+    const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${apiKey}`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const transformedGlobalQuote = removeLeadingIntegersFromKeys(data['Global Quote'])
+        setGlobalQuoteInfo(transformedGlobalQuote);
       })
       .catch(console.log)
   };
+
 
   const securitiesColumns = [
     {
@@ -83,12 +120,56 @@ const App = () => {
     filterType: 'checkbox',
     selectableRows: 'single',
     onRowSelectionChange: newRow => {
-      setSelectedSecuritySymbol(securitiesData[newRow[0].index].symbol);
+      const symbol = securitiesData[newRow[0].index].symbol;
+      setSelectedSecuritySymbol(symbol);
+      getHistoricalPrices(symbol, '5min', setSelectedSecuritySymbol5MinuteData);
+      getHistoricalPrices(symbol, '60min', setSelectedSecuritySymbol60MinuteData);
+      getSMA(symbol, '5min');
+      getGlobalQuote(symbol);
     }
   };
 
-  const selectedSecuritySymbolColumns = [];
-  const selectedSecuritySymbolOptions = {};
+  const selectedSecuritySymbolHistoricalPricesColumns = [
+    {
+      name: "timeStamp",
+      label: "Time Stamp",
+    },
+    {
+      name: "close",
+      label: "Close",
+    },
+    {
+      name: "high",
+      label: "High",
+    },
+    {
+      name: "low",
+      label: "Low",
+    },
+    {
+      name: "open",
+      label: "Open",
+    },
+    {
+      name: "volume",
+      label: "Volume",
+    },
+  ];
+  const selectedSecuritySymbolHistoricalPricesOptions = {};
+
+  const selectedSecuritySymbolIndicatorSMAColumns = [
+    {
+      name: "timeStamp",
+      label: "Time Stamp",
+    },
+    {
+      name: "SMA",
+      label: "Simple Moving Average",
+    },
+  ];
+  const selectedSecuritySymbolIndicatorSMAOptions = {};
+
+
 
   return (
     <>
@@ -100,7 +181,7 @@ const App = () => {
           onChange={event => setKeyword(event.target.value)}
         ></input>
         <button
-          onClick={getSymbols}
+          onClick={getSecurities}
         >{search}</button>
       </form>
       <MUIDataTable
@@ -109,14 +190,43 @@ const App = () => {
         columns={securitiesColumns}
         options={securitiesOptions}
       />
+      {Object.entries(globalQuoteInfo).map(([key, value], index) => {
+        const keyWithoutWhitespace = replaceSpacesWithDashes(key);
+        return (
+          <div key={index} className="global-quote-info__container">
+            <div className={`global-quote-info__${keyWithoutWhitespace}`}>
+              <span className={`global-quote-info__${keyWithoutWhitespace}-label`}>{key}</span>
+              <span className={`global-quote-info__${keyWithoutWhitespace}-value`}>{value}</span>
+            </div>
+          </div>
+        );
+      })}
       <MUIDataTable
-        title={selectedSecuritySymbol}
-        data={selectedSecuritySymbolData}
-        columns={selectedSecuritySymbolColumns}
-        options={selectedSecuritySymbolOptions}
+        title={`5 Minute Prices for ${selectedSecuritySymbol} with SMA`}
+        data={selectedSecuritySymbol5MinuteData}
+        columns={selectedSecuritySymbolHistoricalPricesColumns}
+        options={selectedSecuritySymbolHistoricalPricesOptions}
+      />
+      <MUIDataTable
+        title={`60 Minute Prices for ${selectedSecuritySymbol} with SMA`}
+        data={selectedSecuritySymbol60MinuteData}
+        columns={selectedSecuritySymbolHistoricalPricesColumns}
+        options={selectedSecuritySymbolHistoricalPricesOptions}
+      />
+      <MUIDataTable
+        title="Indicators"
+        data={selectedSecuritySymbolIndicatorSMAData}
+        columns={selectedSecuritySymbolIndicatorSMAColumns}
+        options={selectedSecuritySymbolIndicatorSMAOptions}
       />
     </>
   );
 }
 
 export default App;
+
+
+// IMPROVEMENTS
+  // choose styling approach, either CSS-in-JS (as with material ui), or as separate stylesheets (as with BEM styling, SASS)
+  // polish styling
+  // loading states
